053aef454b2c8877050b2e5e3f09afb8f33b2a74
Joseph Silva <dull.bananas0@gmail.com>
2026-07-02 15:56:51 -0700

    85 questions done

---
+++ backend/questions/__init__.py
@@ -0,0 +1,9 @@
+# formatting:
+# %% means %
+# %he% means he/she (the opposite sex)
+# %him% him/her
+# %his%
+# %bf% means boyfriend/girlfriend
+# %sp% husband/wife/spouse
+
+# todo: look for inconsistencies in style overall
