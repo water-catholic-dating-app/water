9f9cbd8024ddd82dacc6d1135c749478b7faa224
Joseph Silva <dull.bananas0@gmail.com>
2026-08-19 17:09:34 -0700

    it probably works now

--- run-ci-checks-locally.sh
+++ run-ci-checks-locally.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 
-# WARNING: this stops and deletes all Docker containers and volumes, possibly containers unrelated to Water or Duolicious
-
 set -o errexit
 
-# Remove cached images build from dockerfiles in this repository, so backend tests don't use old code. Also remove volumes and containers to prevent test failures caused by persisted state.
-docker stop $(docker ps --quiet) || true
-docker rm --force $(docker ps --quiet --all) || true
-docker volume prune --all --force
-docker compose --file backend/docker-compose.test.yml down --volumes --rmi local
+cd backend
+(docker compose -f docker-compose.test.yml kill 2> /dev/null) || true
+docker compose -f docker-compose.test.yml down
+cd ..
 
-# Prevent rapid disk space exhaustion caused by repeatedly deleting and rebuilding images
-docker system prune --volumes --force
+# Try some or all of these commands if the repeated rebuilding of Docker containers leads to running out of disk space:
+#
+# docker stop $(docker ps --quiet) || true
+# docker rm --force $(docker ps --quiet --all) || true
+# docker volume prune --all --force
+# docker compose --file backend/docker-compose.test.yml down --volumes --rmi local
+# docker system prune --volumes --force
 
-#act -W .github/workflows/frontend-eslint.yml
-#act -W .github/workflows/frontend-type-checks.yml
-#act -W .github/workflows/frontend-playwright.yml --artifact-server-path $PWD/.frontend-playwright-artifacts
-#act -W .github/workflows/frontend-jest.yml
-act -j 'functionality-tests-6' -W .github/workflows/backend-test.yml --concurrent-jobs 1 # Limiting to 1 concurrent job is a workaround to avoid docker container name collisions
+act -W .github/workflows/frontend-eslint.yml
+act -W .github/workflows/frontend-type-checks.yml
+act -W .github/workflows/frontend-playwright.yml --artifact-server-path $PWD/.frontend-playwright-artifacts
+act -W .github/workflows/frontend-jest.yml
+
+# I couldn't get nektos/act to work for backend-test.yml without using ubuntu-latest=-self-hosted, other than the mypy part.
+act -W .github/workflows/backend-test.yml -j 'mypy'
+act -W .github/workflows/backend-test.yml -P ubuntu-latest=-self-hosted -j 'functionality-tests-1'
+act -W .github/workflows/backend-test.yml -P ubuntu-latest=-self-hosted -j 'functionality-tests-2'
+act -W .github/workflows/backend-test.yml -P ubuntu-latest=-self-hosted -j 'functionality-tests-3'
+act -W .github/workflows/backend-test.yml -P ubuntu-latest=-self-hosted -j 'functionality-tests-5'
+act -W .github/workflows/backend-test.yml -P ubuntu-latest=-self-hosted -j 'functionality-tests-6'
+act -W .github/workflows/backend-test.yml -P ubuntu-latest=-self-hosted -j 'unit-tests'
